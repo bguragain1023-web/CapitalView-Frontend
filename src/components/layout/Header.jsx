6b998fd64089f2d5694fb56bcebced4d1c730671
Brazesh Guragain <b.guragain1023@gmail.com>
2026-07-07 20:58:50 +1000

    react icon integrated

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -2,6 +2,9 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
+import { TbLogin } from "react-icons/tb";
+import { IoIosCreate } from "react-icons/io";
+import { IoLogOut } from "react-icons/io5";
 
 export const Header = () => {
   return (
@@ -11,11 +14,15 @@ export const Header = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Link className="nav-link" href="#home">
-              Login
+            <Link className="nav-link" to="/">
+              <TbLogin /> Login
             </Link>
-            <Link className="nav-link" href="#link">
-              Signup
+            <Link className="nav-link" to="/">
+              <IoIosCreate /> Signup
+            </Link>
+            <Link className="nav-link" to="/">
+              <IoLogOut />
+              Logout
             </Link>
           </Nav>
         </Navbar.Collapse>
